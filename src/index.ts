23f5397a4418d94b1eb08da0968c33196c507314
Lenny Reis <142733648+ReisLcb@users.noreply.github.com>
2025-03-14 18:17:28 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import express from "express"; // {} servem para puxar algo específico da bibli
 import readline from "readline"
 import axios from "axios"
 
-const app = express()
-const porta = 3000
+const app = express() // app recebe as funcionalidades da biblioteca express
+const porta = 3000 // Declaramos a porta que o servidor irá rodar
 
-interface Pessoa{
+let id = 0
+
+interface Pessoa{ 
+    readonly id:number
     nome:string
     sexo:string
     quantidade:number
@@ -22,33 +25,62 @@ app.get("/", (request, response) => { // método get é declarado na raíz da AP
     response.send("Servidor iniciado") // Ao entrar no servidor, o servidor envia uma resposta ao usuário: "Servidor iniciado"
 }) // Servidor Criado
 
+// Função do menu, permitindo que o usuario escolha varias opcoes
+// Fica em loop até que seja finalizada selecionando a opção de sair
+// readline (rl) captura as informações passadas pelo usuário
 function showMenu(){
     console.log("====MENU===");
     console.log("1- adicionar pessoa")
-    console.log("2- opção 2")
-    console.log("3- opção 3")
-    console.log("4- opção 4")
+    console.log("2- listar pessoas")
+    console.log("3- remover pessoa")
+    console.log("4 - Sair")
 
-    rl.question("Escolha uma opção: ", async (opcao) => {
+    rl.question("Escolha uma opção: ", async (opcao) => { //usamos async pois precisamos esperar uma resposta da api antes de selecionar uma nova opcao
         switch(opcao){
             case "1":
-                rl.question("Digite o nome da pessoa: ", async (nomeBusca) => {
-                    const {data} = await axios.get(`https://api.genderize.io?name=${nomeBusca}&country_id=BR`)
-                    if(data != null) pessoas.push({nome: data.name, sexo: data.gender, quantidade: Math.floor(data.count * data.probability)})
-                    console.log(pessoas);
-                    
+                rl.question("Digite o nome da pessoa: ", async (nomeBusca) => { 
+                    const {data} = await axios.get(`https://api.genderize.io?name=${nomeBusca}&country_id=BR`) // as respostas GET contem um padrao com varios atributos, {data} é a parte da resposta que nos interessa
+                    if(data != null){
+                        pessoas.push({id: id, nome: data.name, sexo: data.gender, quantidade: Math.floor(data.count * data.probability)})
+                        id++
+                    }
+                    else console.log("Não existem pessoas com este nome")
+                    console.log(`${nomeBusca} foi adicionado com sucesso`);
+                    showMenu()
                 })
-                showMenu()
                 break;
 
             case "2":
-                console.log("Selecionou 2\n");
+                if(pessoas.length > 0) console.log(pessoas)
+                else console.log("A lista está vazia");
                 showMenu()
                 break;
 
             case "3":
-                console.log("Selecionou 3\n");
-                showMenu()
+                let achou = false
+                let posicao = 0
+
+                rl.question("Digite o id da pessoa a ser removida: ", (id) =>{
+                    pessoas.forEach((item) => {
+                        if(item.id == parseInt(id)) achou = true
+                        posicao = pessoas.indexOf(item)
+                    })
+                    
+                    if(achou){
+                        console.log(`${pessoas[posicao].nome} removido`)
+                        pessoas.splice(posicao, 1)
+                    } else console.log("Pessoa não encontrada")
+                    showMenu()
+
+                    /*for (const pessoa of pessoas) {
+                        if(pessoa.id == parseInt(id)){
+                            achou = true
+                            pessoas.splice(pessoas.indexOf(pessoa), 1)
+                            console.log(`${pessoa.nome} removido`)
+                            showMenu()
+                        }
+                    }*/
+                })
                 break;
 
             case "4":
@@ -65,5 +97,5 @@ function showMenu(){
 
 app.listen(porta, () => { // "Ouve" a porta a porta do servidor
     console.log(`Servidor iniciado em http://localhost:${porta}`); // Exibe o link de acesso ao servidor
-    showMenu()
+    showMenu() // Executa a interfaçe ao início do servidor
 })
