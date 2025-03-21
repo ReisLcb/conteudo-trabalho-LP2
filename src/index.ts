e75a6af673e2662b1c74df51a279c9e0170149b1
Lenny Reis <142733648+ReisLcb@users.noreply.github.com>
2025-03-21 22:15:46 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,102 +1,107 @@
-import express from "express"; // {} servem para puxar algo específico da biblioteca, sem {} é puxada a biblioteca inteira
-import readline from "readline"
-import axios from "axios"
+import express from 'express'
+import axios from 'axios'
+import readline from 'readline'
 
-const app = express() // app recebe as funcionalidades da biblioteca express
-const porta = 3000 // Declaramos a porta que o servidor irá rodar
-
-let id = 0
-
-interface Pessoa{ 
-    readonly id:number
-    nome:string
-    sexo:string
-    quantidade:number
-}
-
-let pessoas:Pessoa[] = []
+const app = express()
+const porta = 3000
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+    input: process.stdin,
+    output: process.stdout
+})
 
-app.get("/", (request, response) => { // método get é declarado na raíz da API
-    response.send("Servidor iniciado") // Ao entrar no servidor, o servidor envia uma resposta ao usuário: "Servidor iniciado"
-}) // Servidor Criado
+app.get("/", (request, response) =>{
+    response.send("Servidor iniciado")
+})
 
-// Função do menu, permitindo que o usuario escolha varias opcoes
-// Fica em loop até que seja finalizada selecionando a opção de sair
-// readline (rl) captura as informações passadas pelo usuário
 function showMenu(){
-    console.log("====MENU===");
-    console.log("1- adicionar pessoa")
-    console.log("2- listar pessoas")
-    console.log("3- remover pessoa")
-    console.log("4 - Sair")
+    console.log("=====MENU=====");
+    console.log("Opções:")
+    console.log("1- Exibir países não independentes");
+    console.log("2- Exibir países que iniciam a semana no domingo");
+    console.log("3- Exibir países da África, América do Norte e do Sul, Ásia, Europa e Oceania");
+    console.log("4- Questão 4");
+    console.log("5- Questão 5");
+    console.log("6- Questão 6");
+    console.log("7- Sair")
 
-    rl.question("Escolha uma opção: ", async (opcao) => { //usamos async pois precisamos esperar uma resposta da api antes de selecionar uma nova opcao
+    rl.question("Escolha uma opção: ", async (opcao) =>{
+        const {data} = await axios.get(`https://restcountries.com/v3.1/all`)
+
         switch(opcao){
             case "1":
-                rl.question("Digite o nome da pessoa: ", async (nomeBusca) => { 
-                    const {data} = await axios.get(`https://api.genderize.io?name=${nomeBusca}&country_id=BR`) // as respostas GET contem um padrao com varios atributos, {data} é a parte da resposta que nos interessa
-                    if(data != null){
-                        pessoas.push({id: id, nome: data.name, sexo: data.gender, quantidade: Math.floor(data.count * data.probability)})
-                        id++
-                    }
-                    else console.log("Não existem pessoas com este nome")
-                    console.log(`${nomeBusca} foi adicionado com sucesso`);
-                    showMenu()
-                })
-                break;
+                if(data != null){
+                    let paisesNaoIndependentes:string[] = []
 
-            case "2":
-                if(pessoas.length > 0) console.log(pessoas)
-                else console.log("A lista está vazia");
+                    for (const pais of data) {
+                        if(!pais.independent) paisesNaoIndependentes.push(pais.name.common)
+                    }
+                console.log(paisesNaoIndependentes);
+                }
                 showMenu()
                 break;
 
-            case "3":
-                let achou = false
-                let posicao = 0
+            case "2":
+                if(data != null){
+                    let paisesEncontrados:string[] = []
 
-                rl.question("Digite o id da pessoa a ser removida: ", (id) =>{
-                    pessoas.forEach((item) => {
-                        if(item.id == parseInt(id)) achou = true
-                        posicao = pessoas.indexOf(item)
-                    })
+                    for(const pais of data){
+                        if(pais.startOfWeek == "sunday") paisesEncontrados.push(pais.name.common)
+                    }
                     
-                    if(achou){
-                        console.log(`${pessoas[posicao].nome} removido`)
-                        pessoas.splice(posicao, 1)
-                    } else console.log("Pessoa não encontrada")
-                    showMenu()
+                    console.log(paisesEncontrados);
+                }
+                showMenu()
+                break;
+            
+            case "3":
+                if(data != null){
+                    let paisesAfrica:string[] = []
+                    let paisesAmericaNorte:string[] = []
+                    let paisesAmericaSul:string[] = []
+                    let paisesAsia:string[] = []
+                    let paisesEuropa:string[] = []
+                    let paisesOceania:string[] = []
 
-                    /*for (const pessoa of pessoas) {
-                        if(pessoa.id == parseInt(id)){
-                            achou = true
-                            pessoas.splice(pessoas.indexOf(pessoa), 1)
-                            console.log(`${pessoa.nome} removido`)
-                            showMenu()
+                    for (const pais of data) {
+                        if(pais.region.toLowerCase() == "asia") paisesAsia.push(pais.name.common)
+                        if(pais.region.toLowerCase() == "europe") paisesEuropa.push(pais.name.common)
+                        if(pais.region.toLowerCase() == "africa") paisesAfrica.push(pais.name.common)
+                        if(pais.region.toLowerCase() == "oceania") paisesOceania.push(pais.name.common) 
+                        if(pais.region.toLowerCase() == "americas"){
+                            for (const continente of pais.continents) {
+                                if(continente.toLowerCase() == "north america") paisesAmericaNorte.push(pais.name.common)
+                                if(continente.toLowerCase() == "south america") paisesAmericaSul.push(pais.name.common)
+                            }
                         }
-                    }*/
-                })
+                    }
+                console.log(
+                    `Países da África: `, paisesAfrica, 
+                    `\nPaíses da América do Norte:`, paisesAmericaNorte, 
+                    `\nPaíses da América do Sul:`, paisesAmericaSul, 
+                    `\nPaíses da Ásia:`, paisesAsia, 
+                    `\nPaíses da Europa:`, paisesEuropa, 
+                    `\nPaíses da Oceania:`, paisesOceania
+                );
+                }
+                showMenu()
                 break;
 
-            case "4":
+            case "7":
                 console.log("Saindo...")
                 rl.close()
                 process.exit()
+                break;
 
             default:
-                console.error("Opção inválida\n");
+                console.log("\nEntrada inválida\n");
                 showMenu()
                 break;
         }
     })
 }
 
-app.listen(porta, () => { // "Ouve" a porta a porta do servidor
-    console.log(`Servidor iniciado em http://localhost:${porta}`); // Exibe o link de acesso ao servidor
-    showMenu() // Executa a interfaçe ao início do servidor
+app.listen(porta, () => {
+    console.log(`Servidor iniciado em http://localhost:${porta}\n`);
+    showMenu()
 })
