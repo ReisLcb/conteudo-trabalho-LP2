29295a70f51a9f5f2537861158a95fc6019efda3
Lenny Reis <142733648+ReisLcb@users.noreply.github.com>
2025-03-26 17:15:39 -0300

    Projeto finalizado
    Funções 4, 5 e 6 implementados

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import axios from 'axios'
 import readline from 'readline'
+import { quantidadeFusoHorario } from './paises-controller'
 
 const app = express()
 const porta = 3000
@@ -20,9 +21,9 @@ function showMenu(){
     console.log("1- Exibir países não independentes");
     console.log("2- Exibir países que iniciam a semana no domingo");
     console.log("3- Exibir países da África, América do Norte e do Sul, Ásia, Europa e Oceania");
-    console.log("4- Questão 4");
-    console.log("5- Questão 5");
-    console.log("6- Questão 6");
+    console.log("4- Exibir países que tenham em seus idiomas oficiais o Português");
+    console.log("5- Exibir países com uma determinada quantidade de fuso-horários");
+    console.log("6- Exibir paises que fazem fronteira com a França");
     console.log("7- Sair")
 
     rl.question("Escolha uma opção: ", async (opcao) =>{
@@ -56,12 +57,7 @@ function showMenu(){
             
             case "3":
                 if(data != null){
-                    let paisesAfrica:string[] = []
-                    let paisesAmericaNorte:string[] = []
-                    let paisesAmericaSul:string[] = []
-                    let paisesAsia:string[] = []
-                    let paisesEuropa:string[] = []
-                    let paisesOceania:string[] = []
+                    let paisesAfrica:string[] = [], paisesAmericaNorte:string[] = [], paisesAmericaSul:string[] = [], paisesAsia:string[] = [], paisesEuropa:string[] = [], paisesOceania:string[] = []
 
                     for (const pais of data) {
                         if(pais.region.toLowerCase() == "asia") paisesAsia.push(pais.name.common)
@@ -87,11 +83,49 @@ function showMenu(){
                 showMenu()
                 break;
 
+            case "4":
+                if(data != null){
+                    let paisesLinguaPortugues:string[] = []
+
+                    for (const pais of data) {
+                        for (const key in pais.languages) {
+                            if(pais.languages[key].toLowerCase() == "portuguese") paisesLinguaPortugues.push(pais.name.common)
+                        }
+                    }
+                    console.log(paisesLinguaPortugues);
+                }
+                showMenu()
+                break;
+
+            case "5":
+                if (data != null) {
+                    rl.question("Informe a quantidade de fuso-horários: ", async (qtd) => {
+                        console.log(quantidadeFusoHorario(parseInt(qtd), data));
+                        showMenu()
+                    })
+                }
+                break;
+
+            case "6":
+                if(data != null){
+                    let paisesFronteiraFranca:string[] = []
+
+                    for (const pais of data) {
+                        if(pais.borders != undefined){
+                            for (const fronteira of pais.borders) {
+                                if(fronteira.toLowerCase() == "fra") paisesFronteiraFranca.push(pais.name.common)
+                            }
+                        }
+                    }
+                    console.log(paisesFronteiraFranca);
+                }
+                showMenu()
+                break;
+
             case "7":
                 console.log("Saindo...")
                 rl.close()
                 process.exit()
-                break;
 
             default:
                 console.log("\nEntrada inválida\n");
